5afbe66ddcaa265bbb6602ec49d8d6ba453e5576
Marianne L. Fuglestad <mlfugles@gmail.com>
2019-02-08 16:44:27 +0100

    Add Marianne to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>Øyvind H</p>
+    <p>Marianne</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
