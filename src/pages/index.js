74305a1e23a9000e54736c5a558dadbcbd5f47de
Øyvind Hatland <nestleder@kvarteret.no>
2019-02-08 16:41:22 +0100

    Added my name to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Øyvind H</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
