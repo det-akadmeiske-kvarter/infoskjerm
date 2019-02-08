ea9e6ad28590c3405e249b6a894a5e746f2d15dc
Øyvind Hatland <nestleder@kvarteret.no>
2019-02-08 17:23:09 +0100

    Oppdaterte navnet mitt i index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <p>Øyvind H</p>
+    <p>Øyvind Hatland</p>
     <p>Marianne</p>
     <p>Emil</p>
     <p>Jørgen</p>
