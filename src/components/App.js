0fe2156514f2e8caa8369e832a4b3463359eca56
Øyvind <yvindj4@gmail.com>
2019-03-05 22:35:00 +0100

    Demo av kort

--- src/components/App.js
+++ src/components/App.js
@@ -32,37 +32,17 @@ class App extends Component {
     if (!this.state.eventData) {
       return <div>Loading..</div>;
     }
+    console.log(this.state.eventData)
     return (
       <div>
-        <h1>Kvarteret: Informasjon-Skjerm</h1>
-        {/* <DataGrid
-          url="https://kvarteret.no/infoskjerm-test/fetchxml.php"
-          dataKeys={[
-            "Spillested",
-            "Navn",
-            "Sted",
-            "Dato",
-            "Start Tid",
-            "Slutt Tid",
-            "Type Navn"
-          ]}
-        /> */}
+        <h1 style={{ color: "#8B1C00" }}>Kvarteret: Informasjon-Skjerm</h1>
         <EventCard
+          floor={"Etg1"}
           value1={this.state.eventData[0]["arrangoernavn"]}
           value2={this.state.eventData[0]["dato"]}
           value3={this.state.eventData[0]["typenavn"]}
           value4={this.state.eventData[0]["sted"]}
           content1="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor "
-          value5={this.state.eventData[1]["arrangoernavn"]}
-          value6={this.state.eventData[1]["dato"]}
-          value7={this.state.eventData[1]["typenavn"]}
-          value8={this.state.eventData[1]["sted"]}
-          content2="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor"
-          value9={this.state.eventData[2]["arrangoernavn"]}
-          value10={this.state.eventData[2]["dato"]}
-          value11={this.state.eventData[2]["typenavn"]}
-          value12={this.state.eventData[2]["sted"]}
-          content3="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor"
         />
       </div>
     );
